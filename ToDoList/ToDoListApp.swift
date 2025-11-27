18f4a690235b84cb83f676b8eab0913bb28b1427
Paul Wagstaff <paul@pwfotos.ca>
2025-11-27 09:15:34 -0700

    Initial Commit

---
+++ ToDoList/ToDoListApp.swift
@@ -0,0 +1,17 @@
+//
+//  ToDoListApp.swift
+//  ToDoList
+//
+//  Created by Paul Wagstaff on 2025-11-27.
+//
+
+import SwiftUI
+
+@main
+struct ToDoListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
